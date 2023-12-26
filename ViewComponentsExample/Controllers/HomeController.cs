301136a17ed599c5dd2afd23921c1c26cea97446
VinayBhagat09 <vinay.bhagat@atos.net>
2023-12-26 19:21:06 +0530

    initial commit

---
+++ ViewComponentsExample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
+
+namespace ViewComponentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("about")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("friend-list")]
+        public IActionResult LoadFriendList()
+        {
+            PersonGrid personGrid = new PersonGrid()
+            {
+                GridTitle = "Employess",
+                Persons = new List<Person>(){
+                 new Person (){PersonName = "Yuvaraj" , JobTitle = "System Engineer"},
+                 new Person ()  {PersonName = "Sudhir" , JobTitle = "Consultant"},
+                  new Person () {PersonName = "Sunil" , JobTitle = "Manager"}
+                 }
+            };
+
+            return ViewComponent("Grid",new { grid = personGrid});
+        }
+    }
+}
---
+++ ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
+
+namespace ViewComponentsExample.ViewComponents
+{
+    public class GridViewComponent : ViewComponent
+    {
+       public async Task <IViewComponentResult> InvokeAsync(PersonGrid grid)
+        {
+            PersonGrid model = new PersonGrid()
+            {
+                GridTitle = "Persons List",
+                Persons = new List<Person>
+                {
+                    new Person() { PersonName = "John", JobTitle = "Engineer" },
+                    new Person() { PersonName = "Billu", JobTitle = "Technician" },
+                    new Person() { PersonName = "Sameer", JobTitle = "Donker" }
+                }
+            
+            };
+            ViewData["Grid"] = model;
+            //strongly typed view components
+            return View("Sample",grid); //invoke a partial Views/ Shared/Components/Grid/Default.cshtml
+
+        }
+    }
+}
